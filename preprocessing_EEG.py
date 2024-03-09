101d135d93965bba04660e34d5fafed1de26e47f
Tony <44306764+Tonynanra@users.noreply.github.com>
2024-03-09 05:43:48 -0500

    finished

--- preprocessing_EEG.py
+++ preprocessing_EEG.py
@@ -106,7 +106,7 @@ def save_eeg_data(eeg_filt):
     #EEG data is saved in chunks of 100 windows
     for i in range(0, len(eeg_filt), 100):
         chunk = eeg_filt[i:i+100]
-        np.savez_compressed(new_dir + f'eeg_filt_{i//100}.npz', chunk)
+        np.save(new_dir + f'eeg_filt_{i//100}', chunk)
 
 def main():
     f = open('output.log', 'w')
